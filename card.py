52c22fbeb2509c6d8f7fa61597bb5c76cd10cdce
H Forrest Alexander <h.forrest.alexander@gmail.com>
2013-06-20 01:14:22 -0700

    centered with border. relatively clean code. centered text seems to stay too narrow?

--- card.py
+++ card.py
@@ -6,9 +6,9 @@ import rsvg
 class Card:
     width, height = 256, 384
     font = "Sans"
-    x_border = 16
+    border = 4
     title_y = 128
-    description_y = 256
+    description_y = 192
 
     def __init__(self, title, description):
         im = "Input/" + self.prefix + title + ".svg"
@@ -17,12 +17,18 @@ class Card:
             surface = cairo.SVGSurface(output, self.width, self.height)
             context = cairo.Context(surface)
             handle = rsvg.Handle(file=im)
+            self.drawBorder(context)
             self.printText(context, title, self.title_y)
             self.printText(context, description, self.description_y, 9)
             surface.finish()
 
-    def drawBorder(self):
-        cairo_new_sub_path
+    def drawBorder(self, context):
+        context.rectangle(self.border/2, self.border/2,
+                          self.width - 2 * self.border,
+                          self.height - 2 * self.border)
+        context.set_line_width(self.border)
+        context.set_line_join(cairo.LINE_JOIN_ROUND)
+        context.stroke()
 
     def printText(self, context, text, y_offset, size=18):
         context.move_to(self.width/2, y_offset)
@@ -30,7 +36,7 @@ class Card:
         layout = pangocairo_context.create_layout()
         font = pango.FontDescription(self.font + " " + str(size))
         layout.set_font_description(font)
-        layout.set_width(self.width - self.x_border)
+        layout.set_width((self.width - self.border) * 2)
         layout.set_alignment(pango.ALIGN_CENTER)
         layout.set_text(text)
         context.set_source_rgb(0, 0, 0)
