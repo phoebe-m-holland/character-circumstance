8abef5362d306268820542bf5c3a9f75e4b84745
H Forrest Alexander <h.forrest.alexander@gmail.com>
2013-06-20 19:59:39 -0700

    okay, renders svgs. not in the right place yet

--- card.py
+++ card.py
@@ -16,10 +16,10 @@ class Card:
         with open(out, 'w') as output:
             surface = cairo.SVGSurface(output, self.width, self.height)
             context = cairo.Context(surface)
-            handle = rsvg.Handle(file=im)
             self.drawBorder(context)
-            self.printText(context, title, self.title_y)
-            self.printText(context, description, self.description_y, 9)
+            self.drawText(context, title, self.title_y)
+            self.drawText(context, description, self.description_y, 9)
+            self.drawImage(context, im)
             surface.finish()
 
     def drawBorder(self, context):
@@ -30,7 +30,7 @@ class Card:
         context.set_line_join(cairo.LINE_JOIN_ROUND)
         context.stroke()
 
-    def printText(self, context, text, y_offset, size=18):
+    def drawText(self, context, text, y_offset, size=18):
         context.move_to(self.width/2, y_offset)
         pangocairo_context = pangocairo.CairoContext(context)
         layout = pangocairo_context.create_layout()
@@ -42,6 +42,13 @@ class Card:
         context.set_source_rgb(0, 0, 0)
         pangocairo_context.update_layout(layout)
         pangocairo_context.show_layout(layout)
+
+    def drawImage(self, context, svg_name):
+        try:
+            svg = rsvg.Handle(file=svg_name)
+            svg.render_cairo(context)
+        except:
+            print "GError processing " + svg_name
 
 
 class Object(Card):
